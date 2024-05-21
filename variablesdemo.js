36bc25277207fc683948283da6adadfae192c9f2
VishalGit386 <ervishalsinghgurjar@gmail.com>
2024-05-21 12:08:14 +0530

    add new branch

---
+++ variablesdemo.js
@@ -0,0 +1,10 @@
+// Type and initial vaalue
+var address = "indore";
+var empnme; //defined type
+empnme = "vishal Gurjar"; // initilize variable
+// only the intial value
+var employeename = "Raj";
+//without type and initial value
+var emmployeename;
+emmployeename = "AJAY";
+console.log(emmployeename);
