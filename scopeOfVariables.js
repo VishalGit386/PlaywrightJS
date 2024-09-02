cb462d100993a40a1ca577bbb2c9b1f426781f58
VishalGit386 <ervishalsinghgurjar@gmail.com>
2024-09-02 17:53:20 +0530

    add variable of scopr

---
+++ scopeOfVariables.js
@@ -0,0 +1,10 @@
+//var y =200; //global vaiable
+function somefun() {
+    if (true) {
+        var x = 100;
+        console.log(x);
+    }
+    console.log(x);
+}
+//console.log(x) //error
+//console.log(y) //ok
